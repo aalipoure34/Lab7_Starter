22255e58b36ab233410f6fd633a76e0c3f234da0
aalipoure34 <64985836+aalipoure34@users.noreply.github.com>
2021-11-06 18:23:09 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ self.addEventListener('install', function (event) {
     caches.open(CACHE_NAME)
       .then(function(cache) {
         console.log('Opened cache');
-        return cache.addAll();
+        return cache.addAll([]);
       })
   );
   
